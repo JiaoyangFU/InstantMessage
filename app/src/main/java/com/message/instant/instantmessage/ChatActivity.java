259339c7b064129d62992f37c506add6ba076c3e
Jiaoyang Fu <j45fu@uwaterloo.ca>
2016-11-15 18:58:10 -0500

    add menu to chat Activity

--- app/src/main/java/com/message/instant/instantmessage/ChatActivity.java
+++ app/src/main/java/com/message/instant/instantmessage/ChatActivity.java
@@ -3,6 +3,7 @@ package com.message.instant.instantmessage;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -101,6 +102,13 @@ public class ChatActivity extends AppCompatActivity {
         if (connectListener != null) {
             connectedRef.removeEventListener(connectListener);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
     }
 
     private void detectConnection() {
